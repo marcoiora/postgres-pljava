a6244c31907dcaab9fab9bb24cd912bb71f3fa47
Martin Bednar <mbednar@vendavo.com>
2016-03-23 15:48:18 +0100

    Support for PostgreSQL 9.5

--- docker-entrypoint-initdb.d/01_init_pljava.sql
+++ docker-entrypoint-initdb.d/01_init_pljava.sql
@@ -1,7 +1,8 @@
+SELECT version();
 SET pljava.libjvm_location TO '/usr/lib/jvm/java-8-oracle/jre/lib/amd64/server/libjvm.so';
 ALTER DATABASE postgres SET pljava.libjvm_location FROM CURRENT;
 CREATE EXTENSION pljava;
-SELECT sqlj.install_jar('file:///pljava/pljava-examples/target/pljava-examples-1.6.0-SNAPSHOT.jar', 'examples', true);
+SELECT sqlj.install_jar('file:///pljava/pljava-examples/target/pljava-examples-1.5.0-BETA3.jar', 'examples', true);
 SHOW search_path;
 SELECT sqlj.get_classpath('javatest');
 SELECT sqlj.set_classpath('javatest', 'examples');
